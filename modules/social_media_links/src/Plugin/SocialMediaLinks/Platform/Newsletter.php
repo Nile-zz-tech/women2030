ef35dac1e53f3ea6f4d4f864381173011c17853a
Alex Seif <alex.seif@gmail.com>
2018-01-22 03:34:31 +0200

    Newsletter & Snapchat

---
+++ modules/social_media_links/src/Plugin/SocialMediaLinks/Platform/Newsletter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\social_media_links\Plugin\SocialMediaLinks\Platform;
+
+use Drupal\social_media_links\PlatformBase;
+
+/**
+ * Provides 'instagram' platform.
+ *
+ * @Platform(
+ *   id = "envelope",
+ *   name = @Translation("Newsletter"),
+ *   urlPrefix = "https://women2030.com/",
+ * )
+ */
+class Newsletter extends PlatformBase {}
---
+++ modules/social_media_links/src/Plugin/SocialMediaLinks/Platform/Snapchat.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\social_media_links\Plugin\SocialMediaLinks\Platform;
+
+use Drupal\social_media_links\PlatformBase;
+
+/**
+ * Provides 'instagram' platform.
+ *
+ * @Platform(
+ *   id = "snapchat",
+ *   name = @Translation("Snapchat"),
+ *   urlPrefix = "https://www.snapchat.com/add/",
+ * )
+ */
+class Snapchat extends PlatformBase {}
