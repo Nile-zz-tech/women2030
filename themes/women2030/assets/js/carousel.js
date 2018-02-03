cc1433e3107dfc7c81741cf8d03cbf6f737fe84d
Alex Seif <alex.seif@gmail.com>
2018-02-03 21:31:42 +0200

    Mobile carousel

--- themes/women2030/assets/js/carousel.js
+++ themes/women2030/assets/js/carousel.js
@@ -1,43 +1,49 @@
 (function ($, window, Drupal, drupalSettings) {
 
   "use strict";
-  $(document).ready(function() {
-    if($('.carousel-responsive').length) {
-        $('.carousel-responsive').slick({
-            dots: false,
-            infinite: true,
-            speed: 1000,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            responsive: [{
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            }, {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }, {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }]
-        });
+  $(document).ready(function () {
+    if ($('.carousel-responsive').length) {
+      $('.carousel-responsive').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          }, {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }, {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }]
+      });
+    }
+    if ($(window).width() <= 991) {
+
+      jQuery('#panel_second .container div').first().slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        rtl: true
+      });
     }
   });
 
 
 })(jQuery, this, Drupal, drupalSettings);
-
-
-
-
-
