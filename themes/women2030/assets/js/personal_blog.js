56f8d4d7b5666543d092ee9c5cce2794facb21b9
Alex Seif <alex.seif@gmail.com>
2018-02-21 05:17:22 +0200

    search mobile

--- themes/women2030/assets/js/personal_blog.js
+++ themes/women2030/assets/js/personal_blog.js
@@ -103,7 +103,7 @@
     var $wd = $(window).width();
     if ($wd < 991) {
       $('#block-socialmedialinks').appendTo('.mobile-main-menu');
-      $('#block-women2030-search').appendTo('.mobile-main-menu');
+      $('#block-searchapipagesearchblockform').appendTo('.mobile-main-menu');
       var $langs = $('#block-languageswitcher');
       $langs.appendTo('#header .container');
       $('#block-languageswitcher a[hreflang="en"]').text('en');
