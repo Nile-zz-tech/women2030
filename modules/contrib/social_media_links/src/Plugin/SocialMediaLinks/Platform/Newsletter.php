2399104e8008583e01757a611d01ef88c5498117
Alex Seif <alex.seif@gmail.com>
2018-04-01 11:13:59 +0200

    module customizations and updates

---
+++ modules/contrib/social_media_links/src/Plugin/SocialMediaLinks/Platform/Newsletter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\social_media_links\Plugin\SocialMediaLinks\Platform;
+
+use Drupal\social_media_links\PlatformBase;
+
+/**
+ * Provides 'instagram' platform.
+ *
+ * @Platform(
+ *   id = "envelope",
+ *   name = @Translation("Newsletter"),
+ *   urlPrefix = "http://women2030.com/",
+ * )
+ */
+class Newsletter extends PlatformBase {}
